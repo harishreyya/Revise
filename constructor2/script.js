905f1ab599da5187b8619c4a157641f8c984e4c3
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-05-10 14:35:22 +0530

    ag

---
+++ constructor2/script.js
@@ -0,0 +1,23 @@
+function products(p,q,t,n){
+    this.price = p;
+    this.quantity = q;
+    this.type = t;
+    this.name = n;
+}
+
+let p1 = new products(1000,2,'t-shirt','nike');
+let p2 = new products(1200,3,'pant','puma')
+let p3 = new products(2000,1,'shirt','addidas')
+
+
+console.log(p1,p2,p3);
+
+
+let str = new String('harish');
+console.log(str)
+console.log(str.toString())
+console.log(str.charAt(0))
+
+let str1 = 'harish'
+
+console.log(str1.charAt(0))
