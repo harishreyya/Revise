5db9099c35f223eddecea64ce29e0031ef5d56d9
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-05-12 23:16:12 +0530

    wcwRC

--- test.js
+++ test.js
@@ -5,4 +5,21 @@ var arrToConvert = [[0,0,1],[2,3,3],[4,4,5]];
 const numbers = [[1,2], [2,3], [3,4]];
 const result = [].concat(...arrToConvert);
 
-console.log(result);
+// console.log(result);
+
+let str = "good boy bad girl";
+let str1=""
+// doog yob dab lirg
+// doog yob dab lrig
+for(i=str.length-1;i>=0;i--){
+    str1+= str[i]
+}
+
+
+let arr = string.split(" ");
+res=[]
+
+for(i=arr.length-1;i>=0;i--){
+res.push(arr[i])
+}
+console.log(res.join(" "))
