6ae493ac363d687a5c160d0d1c1fcec6c295b929
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-05-12 22:28:56 +0530

    sdcwsd

---
+++ test.js
@@ -0,0 +1,8 @@
+var arrToConvert = [[0,0,1],[2,3,3],[4,4,5]]; 
+
+// console.log(get1DArray(arrToConvert));
+
+const numbers = [[1,2], [2,3], [3,4]];
+const result = [].concat(...arrToConvert);
+
+console.log(result);
