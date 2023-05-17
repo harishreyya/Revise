ca351919dd3b9f2f6fc3d598e8bbcfa67875f098
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-05-17 14:19:46 +0530

    rasv

--- test.js
+++ test.js
@@ -1,47 +1,58 @@
-var arrToConvert = [[0,0,1],[2,3,3],[4,4,5]]; 
+// var arrToConvert = [[0,0,1],[2,3,3],[4,4,5]]; 
 
 // console.log(get1DArray(arrToConvert));
 
-const numbers = [[1,2], [2,3], [3,4]];
-const result = [].concat(...arrToConvert);
+// const numbers = [[1,2], [2,3], [3,4]];
+// const result = [].concat(...arrToConvert);
 
-console.log(result);
+// console.log(result);
 
-let str = "good boy bad girl";
-let str1=""
+// let str = "good boy bad girl";
+// let str1=""
 // doog yob dab lirg
 // doog yob dab lrig
-for(i=str.length-1;i>=0;i--){
-    str1+= str[i]
-}
+// for(i=str.length-1;i>=0;i--){
+//     str1+= str[i]
+// }
 
 
-let arr = str.split(" ");
-res=[]
-arr.reverse()
-console.log(arr)
-for(i=arr.length-1;i>=0;i--){
-res.push(arr[i])
-}
-console.log(res.join(" "))
+// let arr = str.split(" ");
+// res=[]
+// arr.reverse()
+// console.log(arr)
+// for(i=arr.length-1;i>=0;i--){
+// res.push(arr[i])
+// }
+// console.log(res.join(" "))
 
 
 // var , let & const
 
-for(var i = 0; i< 6; i++){
-   setTimeout(() => {
-    console.log("var",i)
-   }, 2000)
-}
+// for(var i = 0; i< 6; i++){
+//    setTimeout(() => {
+//     console.log("var",i)
+//    }, 2000)
+// }
 
-console.log("------------------")
-for(let i = 0; i< 6; i++){
-    setTimeout(() => {
-        console.log("let",i)
-       }, 2)
-}
-console.log("------------------")
-for( i = 0; i< 6; i++){
-    console.log("no settimeout",i)
+// console.log("------------------")
+// for(let i = 0; i< 6; i++){
+//     setTimeout(() => {
+//         console.log("let",i)
+//        }, 2)
+// }
+// console.log("------------------")
+// for( i = 0; i< 6; i++){
+//     console.log("no settimeout",i)
+// }
+
+console.log(m)
+console.log(n)
+console.log(x)
+ const x = 4
+{
+    let n = 0
+    {
+        var m = 2
+    }
 }
 
