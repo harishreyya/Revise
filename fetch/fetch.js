1009a455e2d1fb4d1aa93980f2ac3ee74e978de1
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-05-26 10:05:25 +0530

    fv

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,9 +1,22 @@
 fetch('https://fakestoreapi.com/products')
 .then((res)=>res.json())
-.then((res)=> 
-
-res.map((data)=>{
-console.log(data.title)
+.then(function(res){
+    console.log(res)
+    showPrdoucts(res)
 })
+ let bigDiv = document.getElementById("container")
+function showPrdoucts(products){
+products.forEach(function(product){
+    let div = document.createElement('div');
+    let img = document.createElement('img');
+    img.src= product.image
+   let title =  document.createElement('p');
+   title.innerText = product.title
+   let price =  document.createElement('p');
+   price.innerText = product.price
 
-)
+   div.append(img,title,price)
+   bigDiv.append(div)
+});
+
+}
